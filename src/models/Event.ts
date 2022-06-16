cad0eea7ea94419308f724ba48bdacfe767d988c
maksimkot <bat.format@gmail.com>
2022-06-17 00:06:31 +0300

    wip

--- src/models/Event.ts
+++ src/models/Event.ts
@@ -1,15 +1,20 @@
 import { IsString, IsNumber } from 'class-validator';
+import { Expose } from 'class-transformer';
 
 export default class Event {
+    @Expose()
     @IsString()
     event_id: string;
 
+    @Expose()
     @IsString()
     platform: string;
 
+    @Expose()
     @IsString()
     environment: string;
 
+    @Expose()
     @IsNumber()
     timestamp: string;
 }
--- src/models/SentryWebhook.ts
+++ src/models/SentryWebhook.ts
@@ -1,23 +1,29 @@
 import { IsString, IsUrl } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Type, Expose } from 'class-transformer';
 import Event from './Event';
 
 export default class SentryWebhook {
+    @Expose()
     @IsString()
     id: string;
 
+    @Expose()
     @IsString()
     project: string;
 
+    @Expose()
     @IsString()
     level: string;
 
+    @Expose()
     @IsString()
     message: string;
 
+    @Expose()
     @IsUrl()
     url: string;
 
+    @Expose()
     @Type(() => Event)
     event: Event;
 }
